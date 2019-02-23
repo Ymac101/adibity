b7227ce545ed3a55d9a55d0a7186bcbb3e461773
Yoav <yoav.haleva@gmail.com>
2019-02-23 12:58:41 +0200

    Centered logo with CSS

--- frontend/index.php
+++ frontend/index.php
@@ -7,6 +7,14 @@ header('Bis: 90', true);
 <head>
   <title>Adi Bity</title>
   <link href="sources/favicon.ico" rel="icon" type="image/x-icon" />
+  <style type="text/css">
+  	.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 75%;
+}
+  </style>
 </head>
 <body style="background-color:black">
 <?php
@@ -14,6 +22,6 @@ if (isset($_GET["xss"])) {
 	echo '<script type="text/javascript">alert(1337)</script>';
 }
 ?>
-<img src="sources/bois_kastel.png">
+<img src="sources/bois_kastel.png" class="center">
 </body>
 </html>
